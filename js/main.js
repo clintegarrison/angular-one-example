e4f56d9ea95f173002aa4e694b97e2dd618fad0e
cxg5940 <clint_garrison@homedepot.com>
2017-05-25 16:17:24 -0400

    added ajax calls

--- js/main.js
+++ js/main.js
@@ -6,23 +6,52 @@ var app = angular.module('myApp', []);
 app.run(function($rootScope) {
 });
 
-app.controller('PlayerController', ['$scope', function($scope) {
-  $scope.playing = false;
-  $scope.audio = document.createElement('audio');
-  $scope.audio.src = './media/Appliance_Kiosk.wav';
-  $scope.play = function() {
-    $scope.audio.play();
-    $scope.playing = true;
-  };
-  $scope.stop = function() {
-    $scope.audio.pause();
-    $scope.playing = false;
-  };
-  $scope.audio.addEventListener('ended', function() {
-    $scope.$apply(function() {
-      $scope.stop()
-    });
+app.controller('PlayerController', ['$scope','$http', function($scope,$http) {
+  var apiKey = 'MDExODQ2OTg4MDEzNzQ5OTM4Nzg5MzFiZA001',
+      nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
+
+  // Hidden our previous section's content
+  // construct our http request
+  $http({
+    method: 'JSONP',
+    url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+  }).success(function(data, status) {
+      console.log(data);
+      $scope.programs = data.list.story;
+  }).error(function(data, status) {
+    // Some error occurred
   });
+
+
+
+
+  $scope.playing = false;
+  var audio = document.createElement('audio');
+  $scope.audio = audio;
+
+  $scope.play = function(program) {
+    if ($scope.playing) audio.pause();
+    var url = program.audio[0].format.mp4.$text;
+    audio.src = url;
+
+    var playPromise = $scope.audio.play();
+
+    if (playPromise !== undefined) {
+      playPromise.then(function() {
+        console.log('yay')
+      }).catch(function(error) {
+        console.log('Chrome is blocking auto play', error)
+      });
+    }
+    // Store the state of the player as playing
+    $scope.playing = true;
+  }
+
+  $scope.actuallyPlay = function(){
+    $scope.audio.play();
+  }
+
+
 }]);
 
 app.controller('RelatedController', ['$scope', function($scope) {
